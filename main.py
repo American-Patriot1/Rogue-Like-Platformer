885833bf8ce544eb517724c9c83c626bc7f6f352
Nathaniel <nathanielhavens5@gmail.com>
2025-02-18 16:26:01 -0600

    created the player class

--- main.py
+++ main.py
@@ -56,13 +56,133 @@ class Surfaces(pygame.sprite.Sprite):
         self.rect.centerx = self.pos_x
         self.rect.centery = self.pos_y
         self.objects_connected = []
+    def activate(self):
+        if len(self.objects_connected) > 0:
+            if self.type == "DOOR":
+                print("pp")
+                for i in self.objects_connected:
+                    self.objects_connected[i].rect.centerx = 500
+                    self.objects_connected[i].rect.centery = 275
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,pos_x,pos_y,width,height):
         super(Player,self).__init__()
-        pass
-
-
+        self.color=(255,0,0,255)
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.width = width
+        self.height = height
+        self.image = pygame.Surface((self.width,self.height), pygame.SRCALPHA)
+        pygame.draw.circle(self.image,self.color,(self.width/2,self.height/2),self.width)
+        self.rect=self.image.get_rect(center=(self.width*2,self.height*2))
+        self.rect.centerx = pos_x
+        self.rect.centery = pos_y
+        self.jump_amt = 0
+        self.jumping = False
+        self.active_jump = False
+        self.grav_amt = 1
+        self.grav_inc = 0.25
+        self.collide_left = False
+        self.collide_right = False
+        self.collide_down = False
+        self.collide_up = False
+        self.health = 100
+    def move(self,keys):
+        if (keys[pygame.K_a]) and (self.collide_left == False):
+            self.rect.centerx -= 10
+        if (keys[pygame.K_d]) and (self.collide_right == False):
+            self.rect.centerx += 10
+        if (keys[pygame.K_SPACE]) and (self.collide_up == False):
+            if (self.collide_down == True):
+                self.grav_amt = 1
+                self.jump_amt = 20
+                self.jumping = True
+                self.active_jump = True
+                self.rect.centery -= self.jump_amt
+                self.jump_amt -= self.grav_amt
+            elif (self.active_jump == True) and (self.jumping == True):
+                self.rect.centery -= self.jump_amt
+                self.jump_amt -= self.grav_amt
+                if self.jump_amt <= 0:
+                    self.jumping = False
+            elif (self.jumping == True):
+                self.active_jump = False
+                self.rect.centery -= self.jump_amt
+                self.jump_amt -= self.grav_amt*2
+                if self.jump_amt <= 0:
+                    self.jumping = False
+        elif (self.jumping == True) and (self.collide_up == False):
+            self.active_jump = False
+            self.rect.centery -= self.jump_amt
+            self.jump_amt -= self.grav_amt*2
+            if self.jump_amt <= 0:
+                self.jumping = False
+        elif (self.jumping == True) and (self.collide_up == True):
+            self.jumping = False
+            self.active_jump = False   
+    def collide(self,group):
+        left_collided = 0
+        right_collided = 0
+        up_collided = 0
+        down_collided = 0
+        for obj in group:
+            if obj.rect.collidepoint(self.rect.centerx-self.width/2+1,self.rect.centery):
+                left_collided += 1
+                if self not in obj.objects_connected:
+                    print("pppp")
+                    obj.objects_connected.append(self)
+            else:
+                try:
+                    obj.objects_connected.remove(self)
+                except:
+                    pass
+            if obj.rect.collidepoint(self.rect.centerx+self.width/2-1,self.rect.centery):
+                right_collided += 1
+                if self not in obj.objects_connected:
+                    obj.objects_connected.append(self)
+            else:
+                try:
+                    obj.objects_connected.remove(self)
+                except:
+                    pass
+            if obj.rect.collidepoint(self.rect.centerx,self.rect.centery-self.height/2+1):
+                up_collided += 1
+                if self not in obj.objects_connected:
+                    obj.objects_connected.append(self)
+            else:
+                try:
+                    obj.objects_connected.remove(self)
+                except:
+                    pass
+            if obj.rect.collidepoint(self.rect.centerx,self.rect.centery+self.height/2-1):
+                down_collided +=1
+                if self not in obj.objects_connected:
+                    obj.objects_connected.append(self)
+            else:
+                try:
+                    obj.objects_connected.remove(self)
+                except:
+                    pass
+        if left_collided == 0:
+            self.collide_left = False
+        else:
+            self.collide_left = True
+        if right_collided == 0:
+            self.collide_right = False
+        else:
+            self.collide_right = True
+        if up_collided == 0:
+            self.collide_up = False
+        else:
+            self.collide_up = True
+        if down_collided == 0:
+            self.collide_down = False
+        else:
+            self.collide_down = True
+    def gravity(self):
+        if (self.jumping == False) and (self.collide_down == False):
+            self.rect.centery += self.grav_amt
+            self.grav_amt += self.grav_inc
 # test_platform = pygame.sprite.Group()
 # test_platform.add(Surfaces(500,525,1000,50,"SURFACE"))
 # test_platform.add(Surfaces(500,25,1000,50,"SURFACE"))
@@ -88,6 +208,9 @@ class Player(pygame.sprite.Sprite):
         
 pp=Room(rooom)
 
+ply=pygame.sprite.Group()
+ply.add(Player(100,300,50,100))
+
 run = True
 while run:
     keys = pygame.key.get_pressed()
@@ -96,7 +219,18 @@ while run:
             run = False
     scr.fill((255,255,255))
     
+    for player in ply:
+        player.collide(pp.room_surfaces)
+        player.move(keys)
+        player.gravity()
+
+    ply.draw(scr)
+
+
     # test_platform.draw(scr)
+
+    for plat in pp.room_surfaces:
+        plat.activate()
 
     pp.room_surfaces.draw(scr)
 
